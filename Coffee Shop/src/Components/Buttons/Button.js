48bc8dd625725d33fdc44f9713f7b72eafe99970
Prasad Adhav <prasadadhav22@gmail.com>
2022-08-01 17:31:46 +0530

    update button component

--- Coffee Shop/src/Components/Buttons/Button.js
+++ Coffee Shop/src/Components/Buttons/Button.js
@@ -2,21 +2,13 @@ import React from "react";
 import "./Button.css";
 
 const Button = () => {
-<<<<<<< HEAD
-    return (
-        <div className=''> <button className="border-2 readmore-btn ">Read More</button>
-            <button className="border-2 viewmore-btn ml-6 ">view More</button></div>
-    )
-}
-=======
   return (
-    <div>
+    <div className="">
       {" "}
       <button className="border-2 readmore-btn ">Read More</button>
       <button className="border-2 viewmore-btn ml-6 ">view More</button>
     </div>
   );
 };
->>>>>>> 0b105fbde53bdff34441eff44515b997326f24f2
 
 export default Button;
