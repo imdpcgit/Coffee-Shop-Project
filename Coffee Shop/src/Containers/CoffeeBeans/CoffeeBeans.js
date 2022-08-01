e22834e224dd8805ad2cc6187d2c077073107795
Imakashkendre <akash.shopisthan@gmail.com>
2022-08-01 20:34:03 +0530

    coffee beans and coffee roast changes

--- Coffee Shop/src/Containers/CoffeeBeans/CoffeeBeans.js
+++ Coffee Shop/src/Containers/CoffeeBeans/CoffeeBeans.js
@@ -28,7 +28,7 @@ const CoffeeBeans = () => {
             </div>
             <img
               alt="ecommerce"
-              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              className="lg:w-1/2 w-full lg:h-auto h-64  object-center rounded"
               src="https://images.unsplash.com/photo-1541167760496-1628856ab772?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1037&q=80"
             />
           </div>
--- Coffee Shop/src/Containers/CoffeeRoast/CoffeeRoast.js
+++ Coffee Shop/src/Containers/CoffeeRoast/CoffeeRoast.js
@@ -10,20 +10,21 @@ const CoffeeRoast = () => {
           <div className="lg:w-4/5 mx-auto flex flex-wrap">
             <img
               alt="ecommerce"
-              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-              src={coffeeroast}
+              className="lg:w-1/2  h-64 lg:h-auto object-center rounded "
+              src="https://images.unsplash.com/photo-1462917882517-e150004895fa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
             />
-            <div className="lg:w-1/2 w-full lg:pl-20 lg:py-6 mt-3 lg:mt-0 ">
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 ">
+              {/* <h2 className="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2> */}
               <img
-                src="https://cdn-icons-png.flaticon.com/512/987/987624.png"
+                src="https://cdn-icons-png.flaticon.com/512/924/924514.png"
                 alt=""
-                className="h-20 pb-5"
+                className="h-11 mb-5 "
               />
-              <h1 className=" text-4xl title-font font-medium mb-1 text-emerald-800">
-                Choose for your <br /> perfect coffee Roast
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+                Choose for your <br /> perfect coffee roast
               </h1>
 
-              <p className="leading-relaxed mt-3 5">
+              <p className="leading-relaxed">
                 Fam locavore kickstarter distillery. Mixtape chillwave tumeric
                 sriracha taximy chia microdosing tilde DIY. XOXO fam indxgo
                 juiceramps cornhole raw denim forage brooklyn. Everyday carry +1
@@ -31,7 +32,8 @@ const CoffeeRoast = () => {
                 pour-over, neutra jean shorts keytar banjo tattooed umami
                 cardigan.
               </p>
-              <div className="mt-5">
+              <br />
+              <div>
                 <Button />
               </div>
             </div>
