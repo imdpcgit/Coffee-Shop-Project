01a5beca1293ff3996163168db7b29808d1e4fb5
Imakashkendre <akash.shopisthan@gmail.com>
2022-08-01 14:10:35 +0530

    banner section created

--- Coffee Shop/src/Components/NavBarCoffee/NavBar.js
+++ Coffee Shop/src/Components/NavBarCoffee/NavBar.js
@@ -6,7 +6,7 @@ const NavBar = () => {
     <>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         <div style={{ width: '75em' }}>
-          <header className="text-gray-600 body-font cursor-pointer border-2 m-8 z-10 ">
+          <header className="text-gray-600 body-font cursor-pointer border-2 m-8 z-50 ">
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
               <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-400	flex flex-wrap items-center text-base justify-center">
                 <a className="mr-5 hover:text-gray-900">Home</a>
@@ -19,7 +19,7 @@ const NavBar = () => {
 
               <a className="mr-5 hover:text-gray-900">Login/Register</a>
               <a className="mr-5 hover:text-gray-900">
-                <img src={{ heartimg }} alt="" />
+                <img src={heartimg} alt="" className="h-4" />
               </a>
             </div>
           </header>
