ef61b44713adf093ecaa3e4f145d67df93147da1
Imakashkendre <akash.shopisthan@gmail.com>
2022-08-02 10:54:46 +0530

    navbar changes

--- Coffee Shop/src/Components/NavBarCoffee/NavBar.js
+++ Coffee Shop/src/Components/NavBarCoffee/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
       <div >
         <div >
           <header className="text-gray-600 body-font  m-8 z-50 bg-[#fff] rounded ">
-            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <div className="container mx-auto flex flex-wrap p-5  md:flex-row items-center nav-flex-in-media">
               <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-gray-400	flex flex-wrap items-center text-base justify-center">
                 <a className="mr-5 hover:text-gray-900 cursor-pointer ">Home</a>
                 <div className="nav-bar-component">
@@ -31,9 +31,12 @@ const NavBar = () => {
               </nav>
 
               <a className="mr-5 hover:text-gray-900 cursor-pointer ">Login/Register</a>
-              <a className="mr-5 hover:text-gray-900 cursor-pointer ">
-                <img src={heartimg} alt="" className="h-4" />
-              </a>
+              <div className='nav-bar-component flex'>
+                <a className="mr-5 hover:text-gray-900 cursor-pointer ">
+                  <img src={heartimg} alt="" className="h-4" />
+                </a>
+              </div>
+
             </div>
           </header>
         </div>
