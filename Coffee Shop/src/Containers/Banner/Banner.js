546d9867b2434168ace742013918b08e874eb28a
Prasad Adhav <prasadadhav22@gmail.com>
2022-08-03 12:15:13 +0530

    updated-code

--- Coffee Shop/src/Containers/Banner/Banner.js
+++ Coffee Shop/src/Containers/Banner/Banner.js
@@ -8,7 +8,7 @@ const Banner = () => {
     <WidthSpecific>
       <div className="flex container justify-center main-div">
         <div className="container px-5 h-200 mx-auto">
-          <div style={{ height: "320px" }}>
+          <div style={{ height: "423px" }}>
             <img
               src="https://images.unsplash.com/photo-1611249021021-21220638532e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
               alt=""
